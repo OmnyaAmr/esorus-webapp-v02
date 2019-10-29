0d5d0fa7f85b22ba4a7ede80d9162d99384d51c1
HeshamOsman <hesham.osman28@yahoo.com>
2019-10-29 20:23:01 +0200

    Redux issue

--- src/config/configureStore.js
+++ src/config/configureStore.js
@@ -9,9 +9,7 @@ const store = createStore(
     rootReducer,
     INITIAL_STATE,
     compose(
-        applyMiddleware(...middleware),
-        window.__REDUX_DEVTOOLS_EXTENSION__ &&
-            window.__REDUX_DEVTOOLS_EXTENSION__()
+        applyMiddleware(...middleware)
     )
 );
 
